fe16eb0d05d2dc75485d96546623a7c22d181e8e
spring <springsquash93@gmail.com>
2023-03-25 00:08:47 -0400

    Add support for embedding executable files.

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ def commands():
 # append data to the object
 @click.command("append")
 @click.argument("targetfile", type=click.Path(exists=True))
-@click.option("-c", "--content", prompt="Enter the content", help="The content to write to the file")
-def append_data(targetfile, content):
-    with open(targetfile, 'ab') as f:
-        f.write(content.encode())
+@click.option("-c", "--content", prompt="Enter the content", help="The content to write to the jpg file")
+@click.option("-t", "--type", type=click.Choice(["txt", "exe"]), default="txt", help="The format of data to append")
+def append_data(targetfile, content, type):
 
-    click.echo(f"Successfully written content to file '{targetfile}'")
+    with open(targetfile, 'ab') as target_file:
+        # Check if user selected type is text input
+        if type == "txt":
+            target_file.write(content.encode())
+
+        # Check if user wants to append file
+        elif type == "exe":
+            with open(content, 'rb') as source_file:
+                target_file.write(source_file.read())
 
 
-# delete data from the object
+    click.echo(f"Successfully written data to file '{targetfile}'")
+
+
+# delete all data from the target file
 @click.command("delete")
 @click.argument("targetfile", type=click.Path(exists=True))
 def delete_data(targetfile):
@@ -28,19 +38,51 @@ def delete_data(targetfile):
         click.echo(f"Successfully deleted all written data from file '{targetfile}'")
 
 
-# Extract the data from jpg files
+# Extract data from jpg files
 @click.command("extract")
 @click.argument("targetfile", type=click.Path(exists=True))
-def extract_jpg(targetfile):
+@click.option("-t", "--type", type=click.Choice(["txt", "exe"]), default="txt", help="The format of data to extract")
+@click.option("-d", "--destination", type=click.Path(exists=True), help="Target destination to store extracted executable")
+def extract_data(targetfile, type, destination):
     with open(targetfile, 'rb') as f:
+
         content = f.read()
         offset = content.index(bytes.fromhex('FFD9'))
         f.seek(offset + 2)
 
-        click.echo(f.read().decode())
+        if type == "txt":
+            try:
+                text = f.read().decode()
+                if text == "":
+                    click.echo("No text found")
+                else:
+                    click.echo(text)
+            except(UnicodeDecodeError):
+                click.echo("Invalid type, see --help")
 
 
-commands.add_command(extract_jpg)
+        elif type == "exe":
+
+            # Default file name
+            target_destination = "extracted_file.exe"
+
+            # Update file name if user provided
+            if destination is not None:
+                target_destination = destination
+
+            # Write bytes to file
+            with open(target_destination, 'wb') as extracted_exe:
+                extracted_exe.write(f.read())
+                click.echo("Written extracted executable")
+
+
+
+
+
+
+
+
+commands.add_command(extract_data)
 commands.add_command(delete_data)
 commands.add_command(append_data)
 
